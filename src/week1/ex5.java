b9378dda6c17bff70accf8d9a0a46a516b93ccbe
sofikin <sofia.kalpin@khk.ee>
2022-09-02 15:15:13 +0300

    "Week 1 Exercise 5"

--- src/week1/ex5.java
+++ src/week1/ex5.java
@@ -2,11 +2,11 @@ package week1;
 
 public class ex5 {
     public static void main(String[] args) {
-        Integer days = 365;
-        Integer hours = 24;
-        Integer minutes = 60;
-        Integer seconds = 60;
-        Integer secondsInYear = days * hours * minutes * seconds;
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+        int secondsInYear = days * hours * minutes * seconds;
         System.out.println("There are " + secondsInYear + " seconds in a year");
     }
 }
