bb2d69c5ea640c5d26f8b7f27c78ba1c8f406105
sofikin <sofia.kalpin@khk.ee>
2022-09-08 16:31:08 +0300

    "Week 1 Exercise 7"

---
+++ src/week1/ex7.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class ex7 {
+    public static void main(String[] args) {
+   int x = 277;
+   int y = 111;
+   int multiplication = x * y;
+
+        System.out.println(x + " * " + y + " = " + multiplication);
+    }
+}
