b95b4cc38d034c94239c058e719ddd82b931cb95
sofikin <sofia.kalpin@khk.ee>
2022-09-08 14:45:36 +0300

    "Week 1 Exercise 4"

--- src/week1/ex4.java
+++ src/week1/ex4.java
@@ -2,6 +2,14 @@ package week1;
 
 public class ex4 {
     public static void main(String[] args) {
-        System.out.println("Chickens:" + "Bacon (kg):" + "A tractor:" + "In a nutshell:");
+        int count = 9000;
+        double kg = 0.1;
+        String tractor = "Zetor";
+
+
+        System.out.println("Chickens: \n" + count);
+        System.out.println("Bacon (kg): \n" + kg);
+        System.out.println("A tractor: \n" + tractor + "\n");
+        System.out.println("In a nutshell: \n" + count + "\n" + kg + "\n" + tractor);
     }
 }
