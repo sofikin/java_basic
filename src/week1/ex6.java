0ffc4ec688ada018be360eed3b5e8748fb0245bc
sofikin <sofia.kalpin@khk.ee>
2022-09-08 15:20:06 +0300

    "Week 1 Exercise 6"

---
+++ src/week1/ex6.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class ex6 {
+    public static void main(String[] args) {
+        int x = 73457;
+        int y = 12888;
+        int sum = x + y;
+
+        System.out.println(x + " + " + y + " = " + sum);
+    }
+}
