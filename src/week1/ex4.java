44af1456065b1c6b6e7af64bf481b3d16cacffc4
sofikin <sofia.kalpin@khk.ee>
2022-09-08 12:39:01 +0300

    "Week 2 Exercise 36 even and odd"

---
+++ src/week1/ex4.java
@@ -0,0 +1,7 @@
+package week1;
+
+public class ex4 {
+    public static void main(String[] args) {
+        System.out.println("Chickens:" + "Bacon (kg):" + "A tractor:" + "In a nutshell:");
+    }
+}
