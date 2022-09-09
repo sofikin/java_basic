650996e6b8c885d188bf46009eebd4ea3d0e6cf2
sofikin <sofia.kalpin@khk.ee>
2022-09-09 14:01:11 +0300

    "Week 1 Exercise 12"

---
+++ src/week1/ex12.java
@@ -0,0 +1,18 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ex12 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String name1 = reader.nextLine();
+        System.out.println("Type your age: ");
+        int age1 = Integer.parseInt(reader.nextLine());
+        System.out.println("Type your name: ");
+        String name2 = reader.nextLine();
+        System.out.println("Type your age: ");
+        int age2 = Integer.parseInt(reader.nextLine());
+        System.out.println(name1 + " and " + name2 + " are " + (age1 + age2) + " years old in total.");
+    }
+}
