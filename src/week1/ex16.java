070d6877410398fc4f25503c131bf4693265e6d5
sofikin <sofia.kalpin@khk.ee>
2022-09-09 15:32:21 +0300

    "Week 1 Exercise 16"

---
+++ src/week1/ex16.java
@@ -0,0 +1,16 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ex16 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        if (number % 2 == 0) {
+            System.out.println("Number " + number + " is even.");
+        } else {
+            System.out.println("Number " + number + " is odd.");
+        }
+    }
+}
