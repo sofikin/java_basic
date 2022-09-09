f2fe5a0a502a46f359838344dabe40a1bdd6e8ba
sofikin <sofia.kalpin@khk.ee>
2022-09-09 13:03:24 +0300

    "Week 1 Exercise 10"

---
+++ src/week1/ex10.java
@@ -0,0 +1,13 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ex10 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type the radius: ");
+        int radius = Integer.parseInt(reader.nextLine());
+        double circumference = Math.PI * 2 * radius;
+        System.out.println("Circumference: of the circle: " + circumference);
+    }
+}
