bb909699722b45d11ef084c1afd9f89745f37feb
sofikin <sofia.kalpin@khk.ee>
2022-09-09 11:43:11 +0300

    "Week 4 Exercise 79"

--- src/week4/ex79.java
+++ src/week4/ex79.java
@@ -1,12 +1,20 @@
 package week4;
 
+import java.util.Scanner;
+
 public class ex79 {
     public static void main(String[] args) {
         NumberStatistics stats = new NumberStatistics();
-        stats.addNumber(3);
-        stats.addNumber(5);
-        stats.addNumber(1);
-        stats.addNumber(2);
+        System.out.println("Type number: ");
+        Scanner reader = new Scanner(System.in);
+        int number;
+        while (true) {
+            number = Integer.parseInt(reader.nextLine());
+            if (number == -1) {
+                break;
+            }
+            stats.addNumber(number);
+        }
         System.out.println("Amount: " + stats.amountOfNumbers());
         System.out.println("Sum: " + stats.sum());
         System.out.println("Average: " + stats.average());
