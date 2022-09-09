f2bc0cfdd10148b185d62414a758cbe36e63d275
sofikin <sofia.kalpin@khk.ee>
2022-09-09 11:45:55 +0300

    "Week 4 Exercise 79"

--- src/week4/ex79.java
+++ src/week4/ex79.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class ex79 {
     public static void main(String[] args) {
         NumberStatistics stats = new NumberStatistics();
+        NumberStatistics all = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
+
         System.out.println("Type number: ");
         Scanner reader = new Scanner(System.in);
         int number;
@@ -13,10 +17,13 @@ public class ex79 {
             if (number == -1) {
                 break;
             }
+            all.addNumber(number);
+            if (number % 2 == 0) {
+                even.addNumber(number); } else {odd.addNumber(number); }
             stats.addNumber(number);
         }
-        System.out.println("Amount: " + stats.amountOfNumbers());
-        System.out.println("Sum: " + stats.sum());
-        System.out.println("Average: " + stats.average());
+        System.out.println("sum: " + all.sum());
+        System.out.println("Sum: " + even.sum());
+        System.out.println("Sum: " + odd.sum());
     }
 }
