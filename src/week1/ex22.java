e9f7189d7dd8081bae70404e82f9c12d8930d9f5
sofikin <sofia.kalpin@khk.ee>
2022-09-15 13:53:34 +0300

    "Week 1 Exercise 22"

---
+++ src/week1/ex22.java
@@ -0,0 +1,21 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ex22 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Type the password: ");
+            String password = reader.nextLine();
+            if (password.equals("carrot")) {
+                System.out.println("Right!");
+                System.out.println("The secret is: jryy qbar!");
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+    }
+}
