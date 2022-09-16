a9398afc7d9f182718963a3bc829bc0c83ca2dbd
sofikin <sofia.kalpin@khk.ee>
2022-09-16 17:52:44 +0300

    "Week 8 Exercise 12.5"

---
+++ src/week8/ShoppingBasket.java
@@ -0,0 +1,24 @@
+package week8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingBasket {
+    private List<Purchase> purchases;
+
+    public ShoppingBasket() {
+        purchases = new ArrayList<Purchase>();
+    }
+
+    public void add(String product, int price){
+        this.purchases.add(new Purchase(product, 1, price));
+    }
+
+    public int price(){
+        int price = 0;
+        for (Purchase purchase : this.purchases){
+            price += purchase.price();
+        }
+        return price;
+    }
+}
--- src/week8/ex12.java
+++ src/week8/ex12.java
@@ -2,11 +2,12 @@ package week8;
 
 public class ex12 {
     public static void main(String[] args) {
-        Purchase purchase = new Purchase("milk", 4, 2);
-        System.out.println( "the total price of a purchase containing four milks is " + purchase.price() );
-        System.out.println( purchase );
-        purchase.increaseAmount();
-        System.out.println( purchase );
-
+        ShoppingBasket basket = new ShoppingBasket();
+        basket.add("milk", 3);
+        basket.add("buttermilk", 2);
+        basket.add("cheese", 5);
+        System.out.println("basket price: " + basket.price());
+        basket.add("computer", 899);
+        System.out.println("basket price: " + basket.price());
     }
 }
