cd621ad2c8369d1c5775e5f8af553675f09e110d
sofikin <sofia.kalpin@khk.ee>
2022-09-16 15:44:48 +0300

    "Week 2 Exercise 29"

---
+++ src/week2/ex29.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class ex29 {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number <= 100) {
+            number++;
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+    }
+}
