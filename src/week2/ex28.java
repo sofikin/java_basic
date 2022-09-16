f8969e3edd5b2de4e858f037ac50fd295b477c45
sofikin <sofia.kalpin@khk.ee>
2022-09-16 14:34:30 +0300

    "Week 2 Exercise 28"

---
+++ src/week2/ex28.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class ex28 {
+    public static void main(String[] args) {
+        int number = 100;
+        while (number >= 1) {
+            System.out.println(number);
+            number--;
+        }
+    }
+}
