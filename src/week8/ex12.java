aa1b39df2a17e3b26fbf2dd34137fc4727759a44
sofikin <sofia.kalpin@khk.ee>
2022-09-16 17:31:58 +0300

    "Week 8 Exercise 12"

--- src/week8/ex12.java
+++ src/week8/ex12.java
@@ -3,12 +3,18 @@ package week8;
 public class ex12 {
     public static void main(String[] args) {
         Storehouse store = new Storehouse();
-        store.addProduct("milk", 3, 10);
-        store.addProduct("coffee", 5, 7);
+        store.addProduct("coffee", 5, 1);
 
-        System.out.println("prices:");
-        System.out.println("milk:  " + store.price("milk"));
-        System.out.println("coffee:  " + store.price("coffee"));
-        System.out.println("sugar: " + store.price("sugar"));
+        System.out.println("stocks:");
+        System.out.println("coffee:  " + store.stock("coffee"));
+        System.out.println("sugar: " + store.stock("sugar"));
+
+        System.out.println("we take a coffee " + store.take("coffee"));
+        System.out.println("we take a coffee " + store.take("coffee"));
+        System.out.println("we take sugar " + store.take("sugar"));
+
+        System.out.println("stocks:");
+        System.out.println("coffee:  " + store.stock("coffee"));
+        System.out.println("sugar: " + store.stock("sugar"));
     }
 }
