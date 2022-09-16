94097480bb2b005bf4224d7f4d8ca41fedffea6a
sofikin <sofia.kalpin@khk.ee>
2022-09-16 17:42:45 +0300

    "Week 8 Exercise 12.4"

--- src/week8/ex12.java
+++ src/week8/ex12.java
@@ -2,16 +2,11 @@ package week8;
 
 public class ex12 {
     public static void main(String[] args) {
-        Storehouse store = new Storehouse();
-        store.addProduct("milk", 3, 10);
-        store.addProduct("coffee", 5, 6);
-        store.addProduct("buttermilk", 2, 20);
-        store.addProduct("jogurt", 2, 20);
-
-        System.out.println("products:");
-        for (String product : store.products()) {
-            System.out.println(product);
-        }
+        Purchase purchase = new Purchase("milk", 4, 2);
+        System.out.println( "the total price of a purchase containing four milks is " + purchase.price() );
+        System.out.println( purchase );
+        purchase.increaseAmount();
+        System.out.println( purchase );
 
     }
 }
