78d13fb8532a39af1a9e2d93e06dc300f6fcdb4f
sofikin <sofia.kalpin@khk.ee>
2022-09-22 16:17:40 +0300

    "Week 2 Exercise 38"

--- src/week2/ex38.java
+++ src/week2/ex38.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class ex38 {
     public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
         System.out.println("How many?");
         int howMany = Integer.parseInt(reader.nextLine());
         int count = 1;
-        while(count <= howMany) {
+        while (count <= howMany) {
             printText();
             count++;
         }
     }
 
-    public static void printText(){
+    public static void printText() {
         System.out.println("In the beginning there were the swamp, the hoe and Java.");
     }
 }
