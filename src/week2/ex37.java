329c772abe7713a17804bf6af236a5037db7d8f6
sofikin <sofia.kalpin@khk.ee>
2022-09-22 16:17:01 +0300

    "Week 2 Exercise 37"

---
+++ src/week2/ex37.java
@@ -0,0 +1,10 @@
+package week2;
+
+public class ex37 {
+    public static void main(String[] args) {
+        printText();
+    }
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+}
