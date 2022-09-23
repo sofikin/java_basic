76435d8be5b1d69e385293532b63467dfb8f9053
sofikin <sofia.kalpin@khk.ee>
2022-09-23 09:57:51 +0300

    "Week 9 Exercise 20.1"

---
+++ src/week9/measurements/ConstantSensor.java
@@ -0,0 +1,31 @@
+package week9.measurements;
+
+public class ConstantSensor implements Sensor {
+    private int measuredValue;
+
+    public ConstantSensor(int measuredValue) {
+        this.measuredValue = measuredValue;
+    }
+
+
+    @Override
+    public int measure() {
+        return this.measuredValue;
+    }
+
+    @Override
+    public boolean isOn() {
+        return true;
+    }
+
+    @Override
+    public void on() {
+
+    }
+
+    @Override
+    public void off() {
+
+    }
+
+}
---
+++ src/week9/measurements/Sensor.java
@@ -0,0 +1,9 @@
+package week9.measurements;
+
+public interface Sensor {
+    boolean isOn();  // returns true if the sensor is on
+    void on();       // switches the sensor on
+    void off();      // switches the sensor off
+    int measure();   // returns the sensor reading if the sensor is on
+    // if the sensor is off, it throws an IllegalStateException
+}
